c1acaf027328a2aaeb864e5d708f123b4798a46a
Thales Morais <thales.morais21@gmail.com>
2020-05-12 21:02:26 -0300

    Adicionado tela home

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
-
+import './home.css'
 export default function(){
 
 
   return(
     <div className="">
-      <h1>Home</h1>
+
+      <div className="headerPage">
+            
+
+            <div className="form">
+              <input type="text"/>
+              <button>Nova Públicação</button>
+            </div>
+
+        
+
+
+      </div>
+
+
+        <div className="card">
+
+        </div>
     </div>
   )
-}
+} 
