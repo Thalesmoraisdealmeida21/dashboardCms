460ac5d8bbdc1a353227a61bd6cf5cc34681bb2e
Thales Morais <thales.morais21@gmail.com>
2020-05-14 21:15:17 -0300

    Adicionado loader no carregamento das paginas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 
 import React, {lazy, Suspense} from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Loader from './assets/loader.svg'
 
+function loadingScreen(){
+  return(
+
+    <div className="loader" style={{textAlign: "center", marginTop:"auto"}}>
+        <img src={Loader} alt=""/>
+        <h3>Carregando</h3>
+    </div>
+  )
+}
 
 const Home = lazy(() => import('./components/pages/home')) 
 const NewPost = lazy(()=> import('./../src/components/pages/newPost/newPost'));
@@ -11,11 +21,11 @@ export default function Routes(){
   return (
       <BrowserRouter>
           <Switch>
-            <Suspense fallback={ <h1> Rendering </h1>}>
+            <Suspense fallback={ (loadingScreen())}>
               <Route path="/" exact component={Home}></Route>
               <Route path="/post/new" exact component={NewPost}></Route>
             </Suspense>
-       
+
               
               
               </Switch>
