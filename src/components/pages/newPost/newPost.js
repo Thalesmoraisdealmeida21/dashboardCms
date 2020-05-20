f0af55cf68d65476eec398e5e326f97bd980706d
Thales Morais <thales.morais21@gmail.com>
2020-05-19 21:11:21 -0300

    Corrigido versão final

--- src/components/pages/newPost/newPost.js
+++ src/components/pages/newPost/newPost.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeaderPage from './../../template/headerPage/headerPage'
-import CKEditor from '@ckeditor/ckeditor5-react';
-import BalloonEditor from '@ckeditor/ckeditor5-build-inline'
+
+import CKEditor from 'ckeditor4-react';
 import { FaFile, FaArrowLeft}from 'react-icons/fa'
 import { success, defaultModules, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -13,20 +13,21 @@ import '@pnotify/confirm/dist/PNotifyConfirm.css'
 
 import api from './../../../services/api'
 
-
 import "./newPost.css"
 import { Link, useHistory } from 'react-router-dom'
 import { useState } from 'react';
+import { useEffect } from 'react';
+
+
 
 export default function(){
-
- 
 
   const [titulo, setTitulo] = useState('');
   const [resumo, setResumo] = useState('');
   const [descricao, setDescricao] = useState('');
-
+  const [imgCapa, setImgCapa] = useState();
   const [modeUpdate, setModeUpdate] = useState(false);
+
 
   const history = useHistory()
 
@@ -37,7 +38,8 @@ export default function(){
         const data = {
           titulo, 
           resumo,
-          descricao
+          descricao,
+          imgCapa
         }
 
 
@@ -47,7 +49,7 @@ export default function(){
             if(ret.status === 201){
               success('Gravou com sucesso !!!!')
               history.push(`./update/${ret.data.id}`)
-             
+              
             }
         
           }).catch((e)=>{
@@ -63,8 +65,30 @@ export default function(){
 
   }
 
+  function uploadImage(event){
+    const imagemCapa = event;
+      
+    const formData = new FormData();
+    const config = {
+      headers: {
+          'content-type': 'multipart/form-data'
+      }
+  };
+  formData.append('capa', imagemCapa);
+
+        api.post('/upload', formData, config).then((ret)=>{
+                setImgCapa(`http://192.168.0.103:3333/${ret.data.pathImg}`)
+        })
+  } 
+
+
   return(
     <div className="">
+      <script src="custom-ckeditor.js"></script>
+        <script src="node_modules/react/react.production.min.js"></script>
+        <script src="node_modules/react-dom/react-dom.production.min.js"></script>
+        <script src="node_modules/ckeditor4-react/dist/ckeditor.js"></script>
+        <script src="app.js"></script>
 
         <HeaderPage pageName="Nova Públicação" icon={()=>{return(<div><Link style={{color: "white"}} to="/"><FaArrowLeft></FaArrowLeft></Link></div>)}} contentHeader={()=>{
           return(<div> <button className="btn" onClick={saveAndPublish}>Salvar e Públicar</button></div>)
@@ -72,38 +96,67 @@ export default function(){
         </HeaderPage>
 
 
-        <div className="card">
-        <form action="container">
+
+        <form action="">
 
 
-        <div className="formGroup">
-          <label>Titulo</label>
-          <input className="inputControl" onChange={(e)=>{setTitulo(e.target.value)}}  value={titulo} type="text"/>
-        </div>
-
-          <div className="formGroup">
-            <label>Resumo</label>
-            <input className="inputControl" type="text" onChange={(e)=>{setResumo(e.target.value)}} value={resumo}/>
-          </div>
-
-
-            <div className="formGroup textAreaContainer">
-           
-              <CKEditor className="editorArtigo"
-                        editor={ BalloonEditor }  
-                        onChange={ ( event, editor ) => {
-                          const data = editor.getData();
-                          setDescricao(data);
-                      } }
-                        data={"Digite seu ártigo aqui !!"}
-                        
-                    />
+          <div className="bodyPost">
+             <div className="formGroup">
+              <label>Titulo</label>
+              <input className="inputControl" onChange={(e)=>{setTitulo(e.target.value)}}  value={titulo} type="text"/>
             </div>
 
+            <div className="formGroup">
+              
+            <div className="containerEditor">
+            
+              <CKEditor className="editorArtigo"
+
+                      onChange={ ( event ) => {
+                          const dataEditor = event.editor.getData();
+                          setDescricao(dataEditor)
+                        
+                    } }
+                    config={ {
+                      height: '50vh'
+                  } }
+                      data={"Digite seu ártigo aqui !!"}
+                      
+                  />
+            </div>
+     
+         </div>
+
+          </div>
+         
+       
+
+                    <div className="menuOptionsPost">
+
+                    <div className="formGroup">
+                        <label>Resumo</label>
+                        <textarea  placeholder="Digite o resumo do seu artigo" className="inputControl" type="text" onChange={(e)=>{setResumo(e.target.value)}} value={resumo}></textarea>
+                    </div>
+
+  
+
+                      <div className="formGroup formImage">
+                          <label>Imagem Capa</label>
+                          <input name="myImage" onChange={(e)=>{uploadImage(e.target.files[0])}} className="inputFile" type="file"/>
+                          <img src={imgCapa} />
+                      </div>
+
+                    </div>
+           
+
+      
+
+
+         
 
 
           </form>
-        </div>
+   
 
 
 
--- src/components/pages/updatePost/updatePost.js
+++ src/components/pages/updatePost/updatePost.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import {useParams} from 'react-router-dom'
 import HeaderPage from './../../template/headerPage/headerPage'
 import CKEditor from '@ckeditor/ckeditor5-react';
-import BalloonEditor from '@ckeditor/ckeditor5-build-inline'
+import BalloonEditor from '@ckeditor/ckeditor5-build-classic'
 import { FaFile, FaArrowLeft}from 'react-icons/fa'
 import { success, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -19,7 +19,26 @@ import "./updatePost.css"
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 
+
+ 
+
 export default function(){
+
+  function uploadImage(event){
+    const imagemCapa = event;
+      
+    const formData = new FormData();
+    const config = {
+      headers: {
+          'content-type': 'multipart/form-data'
+      }
+  };
+  formData.append('capa', imagemCapa);
+  
+        api.post('/upload', formData, config).then((ret)=>{
+                setImgCapa(`http://192.168.0.103:3333/${ret.data.pathImg}`)
+        })
+  }
 
 
   const { id } = useParams(); 
@@ -27,32 +46,45 @@ export default function(){
   const [titulo, setTitulo] = useState('');
   const [resumo, setResumo] = useState('');
   const [descricao, setDescricao] = useState('');
+  const [imgCapa, setImgCapa] = useState();
 
-
+  
 
   const [modeUpdate, setModeUpdate] = useState(false);
 
 
-  function getDataPost(){
+   async function getDataPost(e){
+
+
         
-        api.get(`post/${id}`).then((ret)=>{
+        const resp = await api.get(`post/${id}`).then((ret)=>{
 
             setTitulo(ret.data.titulo)
             setResumo(ret.data.resumo)
             setDescricao(ret.data.descricao)
+            setImgCapa(ret.data.imgCapa)
+            console.log(ret.data.descricao)
+            CKEditor.data = ret.data.descricao
         })
+
+      
+
+        
   }
+
+
   async function saveAndPublish(e){
         e.preventDefault();
 
         const data = {
           titulo, 
           resumo,
-          descricao
+          descricao,
+          imgCapa
         }
 
 
-
+   
         if(modeUpdate){
 
         }else {
@@ -80,38 +112,67 @@ export default function(){
         </HeaderPage>
 
 
-        <div className="card">
-        <form action="container">
+     
+        <form>
+
+          <div className="bodyPost">
+                <div className="formGroup">
+                <label>Titulo</label>
+                <input className="inputControl" onChange={(e)=>{setTitulo(e.target.value)}}  value={titulo} type="text"/>
+              </div>
 
 
-        <div className="formGroup">
-          <label>Titulo</label>
-          <input className="inputControl" onChange={(e)=>{setTitulo(e.target.value)}}  value={titulo} type="text"/>
-        </div>
-
-          <div className="formGroup">
-            <label>Resumo</label>
-            <input className="inputControl" type="text" onChange={(e)=>{setResumo(e.target.value)}} value={resumo}/>
-          </div>
-
-
-            <div className="formGroup textAreaContainer">
-           
-              <CKEditor className="editorArtigo"
-                        editor={ BalloonEditor }  
-                        onChange={ ( event, editor ) => {
-                          const data = editor.getData();
-                          setDescricao(data);
+              <div className="formGroup">
+              
+                <div className="containerEditor">
+                
+                  <CKEditor className="editorArtigo"
+                                  editor={BalloonEditor}
+                          onChange={ ( event,editor ) => {
+                              const dataEditor = editor.getData();
+                              setDescricao(dataEditor)
+                            
+                        } }
+                        config={ {
+                          height: '100vh'
                       } }
-                        data={descricao}
-                        
-                    />
+
+                      data={descricao}  
+                          
+                      />
+                </div>
+       
+           </div>
+
+           
+          </div>
+          <div className="menuOptionsPost">
+            <div className="formGroup">
+              <label>Resumo</label>
+              <input className="inputControl" type="text" onChange={(e)=>{setResumo(e.target.value)}} value={resumo}/>
+            </div>
+
+            <div className="formGroup formImage">
+                          <label>Imagem Capa</label>
+                          <input name="myImage" onChange={(e)=>{uploadImage(e.target.files[0])}} className="inputFile" type="file"/>
+                          <img src={imgCapa} />
+                      </div>
+
             </div>
 
 
+          
+
+
+
+
+    
+
+     
+
 
           </form>
-        </div>
+
 
 
 
